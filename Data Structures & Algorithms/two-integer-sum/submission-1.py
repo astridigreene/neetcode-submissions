53f964174f12face564265a838c84cd830541dc2
Astrid Greene <astridionegreene@gmail.com>
2026-05-09 20:30:28 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i in range(len(nums)):
+            if (target - nums[i]) in d and d[target - nums[i]] != i:
+                return [min(i, d[target - nums[i]]), max(i, d[target - nums[i]])]
+            d[nums[i]] = i
+        return[0,0]
