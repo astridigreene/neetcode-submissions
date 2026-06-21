442c273150ab380637bd7b838fd25d8cc563ed88
Astrid Greene <astridionegreene@gmail.com>
2026-06-21 14:36:24 -0400

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        curr_count = 0
+        eq_val = 0
+        for i, num in enumerate(nums):
+            if i >= len(nums)-eq_val:
+                break
+            if num == val:
+                swap = True
+                while swap and i != len(nums)-eq_val:
+                    eq_val += 1
+                    # swap curr and last(ish)
+                    temp = nums[len(nums)-eq_val]
+                    nums[len(nums)-eq_val] = num
+                    nums[i] = temp
+                    if nums[i] != val:
+                        swap = False
+        return len(nums)-eq_val
