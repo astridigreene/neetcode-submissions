c09b60aa906def515f131e91b0142bdecc348be8
Astrid Greene <astridionegreene@gmail.com>
2026-07-22 10:45:07 -0400

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        HashSet<int> nums_count = new HashSet<int>();
+        foreach (int num in nums) {
+            if (!nums_count.Add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
