1a172edd63b491203a9190b4c02999f50d5da27e
Astrid Greene <astridionegreene@gmail.com>
2026-07-23 18:44:04 -0400

    Add: max-consecutive-ones - submission-2

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-2.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int FindMaxConsecutiveOnes(int[] nums) {
+        int max_count = 0;
+        int curr_count = 0;
+        for (int i = 0; i < nums.Length; ++i) {
+            if (nums[i] == 0) curr_count = 0;
+            else ++curr_count;
+            max_count = Math.Max(max_count, curr_count);
+        }
+        return max_count;
+    }
+}
