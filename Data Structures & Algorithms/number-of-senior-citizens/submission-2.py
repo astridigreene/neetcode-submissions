ba98e9e788205d15171b3bf6366771a3b97f1c36
Astrid Greene <astridionegreene@gmail.com>
2026-09-01 16:53:57 -0400

    Add: number-of-senior-citizens - submission-2

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        # 0-9 (phone), 10 (gender), 11-12 (age), 13-14 (seat)
+        count = 0
+        for i, arr in enumerate(details):
+            if int(arr[11:13]) > 60:
+                count += 1
+        return count
