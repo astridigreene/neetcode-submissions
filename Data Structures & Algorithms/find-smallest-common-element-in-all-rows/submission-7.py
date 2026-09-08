6dc33ac99fc35dc44105a8181eeed91293a22c19
Astrid Greene <astridionegreene@gmail.com>
2026-09-08 14:46:56 -0400

    Add: find-smallest-common-element-in-all-rows - submission-7

---
+++ Data Structures & Algorithms/find-smallest-common-element-in-all-rows/submission-7.py
@@ -0,0 +1,26 @@
+class Solution:
+    def smallestCommonElement(self, mat: List[List[int]]) -> int:
+        counts = defaultdict(int)
+        max_num = mat[0][len(mat[0])-1]
+        for i, row in enumerate(mat):
+            if row[len(row)-1] < max_num:
+                max_num = row[len(row)-1]
+            for j, num in enumerate(row):
+                if num > max_num:
+                    counts.pop(num, None)
+                    break
+                if counts[num] < i:
+                    counts.pop(num, None)
+                    continue
+                counts[num] += 1
+        
+        res = -1
+        for n, c in counts.items():
+            if c == len(mat):
+                if res == -1:
+                    res = n
+                else:
+                    if n < res:
+                        res = n
+        
+        return res
